09cd855c3b7fcee6b2b6c84ff07f286a28f4aae1
Tsagan Garyaeva <simplewebby@outlook.co>
2018-02-07 13:12:00 -0500

    Angular simple app

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 @Injectable()
 export class DataService {
-private goals = new BehaviorSubject<any>(['The initial goal', 'Hello goal']);
+private goals = new BehaviorSubject<any>(['Bananas', 'Oranges']);
 goal = this.goals.asObservable();
 
   constructor() { }
